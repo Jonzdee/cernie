236d6b5e25a49959204b1054c3d5e48cef25e84d
Ogunyankin Johnson Olayemi <ogunyankinjohnson@gmail.com>
2025-04-13 18:57:40 +0100

    upate

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import { BackgroundBeamsDemo } from "./hero";
 import { NavbarDemo } from "./nav";
-import dynamic from 'next/dynamic';
 
-// Dynamically import AppleCardsCarouselDemo to render only on the client
-const AppleCardsCarouselDemo = dynamic(() => import("./carousel"), { ssr: false });
 
 export default function Home() {
+ 
   return (
     <>
       <NavbarDemo />
       <BackgroundBeamsDemo />
-      <AppleCardsCarouselDemo />
+      
     </>
   );
 }
